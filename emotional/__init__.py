42b5a83536ba5e2478fe537c2f9010d6b1335193
Axel H. <noirbizarre@users.noreply.github.com>
2023-12-07 22:42:01 +0100

    build(version): use a simpler way to fetch version from `VERSION`

--- emotional/__init__.py
+++ emotional/__init__.py
@@ -1,18 +1,14 @@
 from __future__ import annotations
 
 from importlib.metadata import PackageNotFoundError, version
-
-try:
-    from importlib.resources import files
-except ImportError:
-    from importlib_resources import files  # type: ignore[no-redef,import-not-found]
+from pathlib import Path
 
 
 def read_version() -> str:
     try:
         return version("emotional")
     except (PackageNotFoundError, ImportError):
-        version_file = files(__package__) / "VERSION"
+        version_file = Path(__file__).parent / "VERSION"
         return version_file.read_text() if version_file.is_file() else "0.0.0.dev"
 
 
